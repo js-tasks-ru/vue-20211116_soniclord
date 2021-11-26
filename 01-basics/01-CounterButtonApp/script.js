69ac2dd1102af2efc98d8cded5e8a9ce72e29c73
Viktor Malakhov <soniclord@mail.ru>
2021-11-26 16:37:21 +1000

    second

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,14 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+const RootComponent = defineComponent ({
+    name: 'Root'
+    , data() {
+        return {
+            count: 0
+        }
+    }
+});
+
+const App = createApp(RootComponent);
+const vm = App.mount('#app');
