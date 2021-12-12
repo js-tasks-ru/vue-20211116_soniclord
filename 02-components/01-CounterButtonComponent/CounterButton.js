a85b3472b22e896655318c03f797d751337ecb25
Viktor Malakhov <soniclord@mail.ru>
2021-12-12 19:12:06 +1000

    counter

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -4,9 +4,20 @@ export default defineComponent({
   name: 'CounterButton',
 
   // Компонент должен иметь входной параметр
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
+
+  methods: {
+    incCount() {
+      let val = this.count+1;
+      this.$emit('update:count', val);
+    },
+  },
 
   // Шаблон лучше держать максимально простым, а логику выносить в методы
-
-  // Шаблон потребуется отредактировать
-  template: `<button type="button">1</button>`,
+  template: `<button type="button" @click="incCount">{{ count }}</button>`,
 });
