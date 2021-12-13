976ead51b629897ab3baadff6c2b691b9cdda4bd
Viktor Malakhov <soniclord@mail.ru>
2021-12-13 14:01:06 +1000

    MeetupInfo/MeetupInfo

--- 02-components/04-MeetupInfo/MeetupInfo.js
+++ 02-components/04-MeetupInfo/MeetupInfo.js
@@ -3,19 +3,44 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 export default defineComponent({
   name: 'MeetupInfo',
 
+  props: {
+    date: {
+      type: Number,
+      require: true,
+    },
+    organizer: {
+      type: String,
+      require: true,
+    },
+    place: {
+      type: String,
+      require: true,
+    },
+  },
+
+  methods: {
+    convertDateFull(timestamp) {
+      let date = new Date(timestamp);
+      return date.toLocaleDateString(navigator.language, { year: 'numeric', month: 'long', day: 'numeric' });
+    },
+    convertDateShort(timestamp) {
+      return new Date(timestamp).toISOString().substr(0, 10);
+    },
+  },
+
   template: `
     <ul class="meetup-info">
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
+        {{ organizer }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
+        {{ place }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
+        <time :datetime="convertDateShort(date)">{{ convertDateFull(date) }}</time>
       </li>
     </ul>`,
 });
