ad3061ca286297319713d994be5eabdd8594de5a
Viktor Malakhov <soniclord@mail.ru>
2021-12-15 18:06:58 +1000

    fix

--- 03-sfc/01-VBindCover/components/MeetupCover.vue
+++ 03-sfc/01-VBindCover/components/MeetupCover.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="meetup-cover" :style="image && `--bg-url: url('${image}')`">
+  <div class="meetup-cover">
     <h1 class="meetup-cover__title">{{ title }}</h1>
   </div>
 </template>
