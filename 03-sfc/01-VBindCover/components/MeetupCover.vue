e185147b32c69c9eb0e34cf5a1b070a18dd61c2f
Viktor Malakhov <soniclord@mail.ru>
2021-12-15 18:03:59 +1000

    Cover2

--- 03-sfc/01-VBindCover/components/MeetupCover.vue
+++ 03-sfc/01-VBindCover/components/MeetupCover.vue
@@ -1,12 +1,27 @@
 <template>
-  <div class="meetup-cover">
-    <h1 class="meetup-cover__title">Title</h1>
+  <div class="meetup-cover" :style="image && `--bg-url: url('${image}')`">
+    <h1 class="meetup-cover__title">{{ title }}</h1>
   </div>
 </template>
 
 <script>
 export default {
   name: 'MeetupCover',
+  props: {
+    title: {
+      type: String,
+      require: false,
+    },
+    image: {
+      type: String,
+      require: false,
+    },
+  },
+  computed: {
+    imgLink(){
+      return this.image ? `url('${this.image}')` : 'var(--default-cover)';
+    }
+  }
 };
 </script>
 
@@ -14,9 +29,9 @@ export default {
 .meetup-cover {
   background-size: cover;
   background-position: center;
-  background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),
-    url('https://course-vue.javascript.ru/api/images/2');
-  /* background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), var(--default-cover); */
+  background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), v-bind(imgLink);
+  /*  url('https://course-vue.javascript.ru/api/images/2'); */
+  /*background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), var(--default-cover); */
   display: flex;
   flex-direction: column;
   align-items: center;
